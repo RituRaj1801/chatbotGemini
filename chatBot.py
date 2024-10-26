187908bbd828e6c7a4d8af6bbf26da9692e64b49
RituRaj1801 <rituraj995kumar@gmail.com>
2024-10-26 12:11:42 +0530

    api-key

--- chatBot.py
+++ chatBot.py
@@ -1,8 +1,10 @@
 import google.generativeai as genai
 import os
 
-os.environ["KEY"] = "AIzaSyA8arRfdpDVfkDwKPNO4QSnrpEOpOlk__4"
-genai.configure(api_key=os.environ["KEY"])
+# os.environ["KEY"] = "AIzaSyA8arRfdpDVfkDwKPNO4QSnrpEOpOlk__4"
+api_key = os.environ.get('API_KEY')
+
+genai.configure(api_key)
 
 model = genai.GenerativeModel("models/gemini-pro")
 
